f13b802ef8a3246c19eaf7314331b0f9e35f983e
iwata1107 <iwata.s1107@gmail.com>
2023-02-11 15:31:43 +0900

    a
    a

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,10 +6,10 @@ import Vapor
 public func configure(_ app: Application) throws {
 
     app.databases.use(.postgres(
-        hostname: "localhost",
-        username: "postgres",
-        password: "",
-        database: "iwatashotadb"
+        hostname: "ec2-3-223-213-207.compute-1.amazonaws.com",
+        username: "ajpozesbvfwgzc",
+        password: "f0240ec3e01e3512f0e62719341786d708e1f532cc42c4d6370db7fd44b0bf35",
+        database: "d2322jtrglsqho"
     ), as: .psql)
 
     // マイグレーションの実行
